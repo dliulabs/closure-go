520937cc2d68a97ca64a521b3c6a67ddef60f99f
david.s.liu@gmail.com <david.s.liu@gmail.com>
2021-02-22 21:10:27 -0600

    update multiple-middleware

--- multiple-middleware/multipleMiddleware.go
+++ multiple-middleware/multipleMiddleware.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"encoding/json"
-	//"fmt"
 	"log"
 	"net/http"
-	//"strconv"
-	//"time"
+	"strconv"
+	"time"
 )
 
 type City struct {
@@ -33,9 +32,34 @@ func mainLogic(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// Pre-Request Middleware
+func filterContentType(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Currently in the check content type middleware")
+		if r.Header.Get("Content-Type") != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Write([]byte("415 - Unsupported Media Type, please send application/json"))
+			return
+		}
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func setServerTimeCookie(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+		// Setting cookie to each and every response
+		cookie := http.Cookie{Name: "Server-Time(UTC)", Value: strconv.FormatInt(time.Now().Unix(), 10)}
+		http.SetCookie(w, &cookie)
+		log.Printf("Cookie: %s, Value: %v", cookie.Name, cookie.Value)
+		log.Println("Currently in the set server time middleware")
+	})
+}
+
 func main() {
-	//mainLogicHandler := http.HandlerFunc(mainLogic)
-	http.HandleFunc("/city", mainLogic)
+	mainLogicHandler := http.HandlerFunc(mainLogic)
+	// http.Handle("/city", filterContentType(mainLogicHandler))
+	http.Handle("/city", filterContentType(setServerTimeCookie(mainLogicHandler)))
 	log.Println(`curl -H "Content-Type: application/json" -X POST http://localhost:8000/city -d '{"name":"New York", "area":304}'`)
 	log.Println(`curl -H "Content-Type: application/json" -X POST http://localhost:8000/city -d '{"name":"Boston", "area":89}'`)
 	log.Println(`curl -H "Content-Type: application/json" -X PATCH http://localhost:8000/city -d '{"name":"Boston", "area":89}'`)
